6ff101eda796c8fc1adad205482e7b254bb2bbf9
TeoNB <tensbi@alumnatflorida.es>
2026-02-28 21:49:13 +0100

    Escena

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+public class Player : MonoBehaviour
+{
+
+	public 
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+    {
+        GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
