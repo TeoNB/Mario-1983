c1e9692d06cfe9ae202e726db01249ab617e7b69
TeoNB <tensbi@alumnatflorida.es>
2026-03-01 22:39:18 +0100

    Movimiento
    Le he puesto el movimiento al Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,23 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-
-	public 
+    Rigidbody2D rb;
+    Vector2 Move;
+    public float velocidad;
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
     {
-        GetComponent<Player>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.linearVelocity = new Vector2(Move.x * velocidad, 0);
+    }
+
+    void OnMove(InputValue value)
+    {
+        Move = value.Get<Vector2>();
     }
 }
